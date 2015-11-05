bde766fad2cb0effd11d25bec1ea8b5835969fc1
Matthieu Gaillet <matthieu@gaillet.be>
2015-11-05 18:34:04 +0100

    Cosmetic changes
    Also added libsigrok-internal.h with structures need for dtm0660

--- src/dmm/dtm0660.c
+++ src/dmm/dtm0660.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2012 Uwe Hermann <uwe@hermann-uwe.de>
  * Copyright (C) 2012 Alexandru Gagniuc <mr.nuke.me@gmail.com>
- * Copyright (C) 2015 Matthieu Gaillet <matthieu@gaillet.be> 
+ * Copyright (C) 2015 Matthieu Gaillet <matthieu@gaillet.be>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,61 +29,7 @@
  *  - Communication parameters: Unidirectional, 2400/8n1
  *  - Protocol looks closely to the fs9721 but with 15 bytes and reversed nibbles.
  *
- * Decoding table :
- *
- * +======+==========+=============+=========+=========+============+
- * | Byte | Bits 7-4 |    Bit 3    |  Bit 2  |  Bit 1  |   Bit 0    |
- * +======+==========+=============+=========+=========+============+
- * |    0 | 0x1      | RS232       | Auto    | DC      | AC         |
- * +------+----------+-------------+---------+---------+------------+
- * |    1 | 0x2      | 4A          | 4F      | 4E      | - (minus)  |
- * +------+----------+-------------+---------+---------+------------+
- * |    2 | 0x3      | 4B          | 4G      | 4C      | 4D         |
- * +------+----------+-------------+---------+---------+------------+
- * |    3 | 0x4      | 3A          | 3F      | 3E      | DP1        |
- * +------+----------+-------------+---------+---------+------------+
- * |    4 | 0x5      | 3B          | 3G      | 3C      | 3D         |
- * +------+----------+-------------+---------+---------+------------+
- * |    5 | 0x6      | 2A          | 2F      | 2E      | DP2        |
- * +------+----------+-------------+---------+---------+------------+
- * |    6 | 0x7      | 2B          | 2G      | 2C      | 2D         |
- * +------+----------+-------------+---------+---------+------------+
- * |    7 | 0x8      | 1A          | 1F      | 1E      | DP3        |
- * +------+----------+-------------+---------+---------+------------+
- * |    8 | 0x9      | 1B          | 1G      | 1C      | 1D         |
- * +------+----------+-------------+---------+---------+------------+
- * |    9 | 0xa      | Diode       | k       | n       | u          |
- * +------+----------+-------------+---------+---------+------------+
- * |   10 | 0xb      | Beep        | M       |  %      | m          |
- * +------+----------+-------------+---------+---------+------------+
- * |   11 | 0xc      | Hold        | Rel     | Ohms    | Farads     |
- * +------+----------+-------------+---------+---------+------------+
- * |   12 | 0xd      | Low battery | Hz      | V       | A          |
- * +------+----------+-------------+---------+---------+------------+
- * |   13 | 0xe      | c2c1_00     | c2c1_01 | Celcius | Fahrenheit |
- * +------+----------+-------------+---------+---------+------------+
- * |   14 | 0xf      | Max         | Min-Max | Min     | Auto-Off   |
- * +------+----------+-------------+---------+---------+------------+
  */
-
-//  LCD truth table 
-//  Segments denomination follows the standardised definition (see https://en.wikipedia.org/wiki/Seven-segment_display)
-//  
-//  +--------------------------------+
-//  |Digit|A|F|E|(n/a)|B|G|C|D|Result|
-//  +--------------------------------+
-//  |0    |1|1|1|0    |1|0|1|1|0xeb  |
-//  |1    |0|0|0|0    |1|0|1|0|0x0a  |
-//  |2    |1|0|1|0    |1|1|0|1|0xad  |
-//  |3    |1|0|0|0    |1|1|1|1|0x8f  |
-//  |4    |0|1|0|0    |1|1|1|0|0x4e  |
-//  |5    |1|1|0|0    |0|1|1|1|0xc7  |
-//  |6    |1|1|1|0    |0|1|1|1|0xe7  |
-//  |7    |1|0|0|0    |1|0|1|0|0x8a  |
-//  |8    |1|1|1|0    |1|1|1|1|0xef  |
-//  |9    |1|1|0|0    |1|1|1|1|0xcf  |
-//  |L    |0|1|1|0    |0|0|0|1|0x61  |
-//  +--------------------------------+
 
 
 #include <config.h>
@@ -99,52 +45,52 @@
 static int parse_digit(uint8_t b)
 {
 	switch (b) {
-	case 0xeb:
-		return 0;
-	case 0x0a:
-		return 1;
-	case 0xad:
-		return 2;
-	case 0x8f:
-		return 3;
-	case 0x4e:
-		return 4;
-	case 0xc7:
-		return 5;
-	case 0xe7:
-		return 6;
-	case 0x8a:
-		return 7;
-	case 0xef:
-		return 8;
-	case 0xcf:
-		return 9;
-	default:
-		sr_dbg("Invalid digit byte: 0x%02x.", b);
-		return -1;
+		case 0xeb:
+			return 0;
+		case 0x0a:
+			return 1;
+		case 0xad:
+			return 2;
+		case 0x8f:
+			return 3;
+		case 0x4e:
+			return 4;
+		case 0xc7:
+			return 5;
+		case 0xe7:
+			return 6;
+		case 0x8a:
+			return 7;
+		case 0xef:
+			return 8;
+		case 0xcf:
+			return 9;
+		default:
+			sr_dbg("Invalid digit byte: 0x%02x.", b);
+			return -1;
 	}
 }
 
 static gboolean sync_nibbles_valid(const uint8_t *buf)
 {
 	int i;
-
+	
 	/* Check the synchronization nibbles, and make sure they all match. */
 	for (i = 0; i < DTM0660_PACKET_SIZE; i++) {
 		if (((buf[i] >> 4) & 0x0f) != (i + 1)) {
 			sr_dbg("Sync nibble in byte %d (0x%02x) is invalid.",
-			       i, buf[i]);
+				   i, buf[i]);
 			return FALSE;
 		}
 	}
-
+	
 	return TRUE;
 }
 
 static gboolean flags_valid(const struct dtm0660_info *info)
 {
 	int count;
-
+	
 	/* Does the packet have more than one multiplier? */
 	count = 0;
 	count += (info->is_nano) ? 1 : 0;
@@ -156,7 +102,7 @@ static gboolean flags_valid(const struct dtm0660_info *info)
 		sr_dbg("More than one multiplier detected in packet.");
 		return FALSE;
 	}
-
+	
 	/* Does the packet "measure" more than one type of value? */
 	count = 0;
 	count += (info->is_hz) ? 1 : 0;
@@ -169,19 +115,19 @@ static gboolean flags_valid(const struct dtm0660_info *info)
 		sr_dbg("More than one measurement type detected in packet.");
 		return FALSE;
 	}
-
+	
 	/* Both AC and DC set? */
 	if (info->is_ac && info->is_dc) {
 		sr_dbg("Both AC and DC flags detected in packet.");
 		return FALSE;
 	}
-
+	
 	/* RS232 flag not set? */
 	if (!info->is_rs232) {
 		sr_dbg("No RS232 flag detected in packet.");
 		return FALSE;
 	}
-
+	
 	return TRUE;
 }
 
@@ -190,48 +136,48 @@ static int parse_value(const uint8_t *buf, float *result)
 	int i, sign, intval = 0, digits[4];
 	uint8_t digit_bytes[4];
 	float floatval;
-
+	
 	/* Byte 1 contains sign in bit 0 */
 	sign = ((buf[1] & (1 << 0)) != 0) ? -1 : 1;
-
+	
 	/*
 	 * Bytes 1-8: Value (4 decimal digits, sign, decimal point)
 	 *
 	 * Over limit: "0L" (LCD), 0x00 0x7d 0x68 0x00 (digit bytes).
 	 */
-
+	
 	/* Merge the two nibbles for a digit into one byte. */
 	for (i = 0; i < 4; i++) {
 		digit_bytes[i] = ((buf[1 + (i * 2)] & 0x0f) << 4);
 		digit_bytes[i] |= (buf[1 + (i * 2) + 1] & 0x0f);
-
+		
 		/* Bit 4 in the byte is not part of the digit. */
 		digit_bytes[i] &= ~(1 << 4);
 	}
-
+	
 	/* Check for "OL". */
 	if (digit_bytes[0] == 0x00 && digit_bytes[1] == 0xeb &&
-	    digit_bytes[2] == 0x61 && digit_bytes[3] == 0x00) {
+		digit_bytes[2] == 0x61 && digit_bytes[3] == 0x00) {
 		sr_spew("Over limit.");
 		*result = INFINITY;
 		return SR_OK;
 	}
-
+	
 	/* Parse the digits. */
 	for (i = 0; i < 4; i++)
 		digits[i] = parse_digit(digit_bytes[i]);
 	sr_spew("Digits: %02x %02x %02x %02x (%d%d%d%d).",
 		digit_bytes[0], digit_bytes[1], digit_bytes[2], digit_bytes[3],
 		digits[0], digits[1], digits[2], digits[3]);
-
+	
 	/* Merge all digits into an integer value. */
 	for (i = 0; i < 4; i++) {
 		intval *= 10;
 		intval += digits[i];
 	}
-
+	
 	floatval = (float)intval;
-
+	
 	/* Decimal point position. */
 	if ((buf[3] & 0x01) != 0) {
 		floatval /= 1000;
@@ -245,14 +191,14 @@ static int parse_value(const uint8_t *buf, float *result)
 	} else {
 		sr_spew("No decimal point in the number.");
 	}
-
+	
 	/* Apply sign. */
 	floatval *= sign;
-
+	
 	sr_spew("The display value is %f.", floatval);
-
+	
 	*result = floatval;
-
+	
 	return SR_OK;
 }
 
@@ -263,49 +209,49 @@ static void parse_flags(const uint8_t *buf, struct dtm0660_info *info)
 	info->is_dc         = (buf[0] & (1 << 1)) != 0;
 	info->is_auto       = (buf[0] & (1 << 2)) != 0;
 	info->is_rs232      = (buf[0] & (1 << 3)) != 0;
-
+	
 	/* Byte 1: LCD SEG2 */
 	info->is_sign       = (buf[1] & (1 << 0)) != 0;
-
+	
 	/* Byte 9: LCD SEG10 */
 	info->is_micro      = (buf[9] & (1 << 0)) != 0;
 	info->is_nano       = (buf[9] & (1 << 1)) != 0;
 	info->is_kilo       = (buf[9] & (1 << 2)) != 0;
 	info->is_diode      = (buf[9] & (1 << 3)) != 0;
-
+	
 	/* Byte 10: LCD SEG11 */
 	info->is_milli      = (buf[10] & (1 << 0)) != 0;
 	info->is_percent    = (buf[10] & (1 << 1)) != 0;
 	info->is_mega       = (buf[10] & (1 << 2)) != 0;
 	info->is_beep       = (buf[10] & (1 << 3)) != 0;
-
+	
 	/* Byte 11: LCD SEG12 */
 	info->is_farad      = (buf[11] & (1 << 0)) != 0;
 	info->is_ohm        = (buf[11] & (1 << 1)) != 0;
 	info->is_rel        = (buf[11] & (1 << 2)) != 0;
 	info->is_hold       = (buf[11] & (1 << 3)) != 0;
-
+	
 	/* Byte 12: LCD SEG13 */
 	info->is_ampere     = (buf[12] & (1 << 0)) != 0;
 	info->is_volt       = (buf[12] & (1 << 1)) != 0;
 	info->is_hz         = (buf[12] & (1 << 2)) != 0;
 	info->is_bat        = (buf[12] & (1 << 3)) != 0;
-
+	
 	/* Byte 13: LCD SEG14  */
 	info->is_degf       = (buf[13] & (1 << 0)) != 0;
 	info->is_degc       = (buf[13] & (1 << 1)) != 0;
 	info->is_c2c1_00    = (buf[13] & (1 << 2)) != 0;
 	info->is_c2c1_01    = (buf[13] & (1 << 3)) != 0;
-    
-    /* Byte 14: LCD SEG15  */
-    info->is_apo        = (buf[14] & (1 << 0)) != 0;
-    info->is_min        = (buf[14] & (1 << 1)) != 0;
-    info->is_minmax     = (buf[14] & (1 << 2)) != 0;
-    info->is_max        = (buf[14] & (1 << 3)) != 0;
+	
+	/* Byte 14: LCD SEG15  */
+	info->is_apo        = (buf[14] & (1 << 0)) != 0;
+	info->is_min        = (buf[14] & (1 << 1)) != 0;
+	info->is_minmax     = (buf[14] & (1 << 2)) != 0;
+	info->is_max        = (buf[14] & (1 << 3)) != 0;
 }
 
 static void handle_flags(struct sr_datafeed_analog_old *analog, float *floatval,
-			 const struct dtm0660_info *info)
+						 const struct dtm0660_info *info)
 {
 	/* Factors */
 	if (info->is_nano)
@@ -318,7 +264,7 @@ static void handle_flags(struct sr_datafeed_analog_old *analog, float *floatval,
 		*floatval *= 1000;
 	if (info->is_mega)
 		*floatval *= 1000000;
-
+	
 	/* Measurement modes */
 	if (info->is_volt) {
 		analog->mq = SR_MQ_VOLTAGE;
@@ -352,18 +298,18 @@ static void handle_flags(struct sr_datafeed_analog_old *analog, float *floatval,
 	if (info->is_percent) {
 		analog->mq = SR_MQ_DUTY_CYCLE;
 		analog->unit = SR_UNIT_PERCENTAGE;
-    }
-
-    if (info->is_degc) {
-        analog->mq = SR_MQ_TEMPERATURE;
-        analog->unit = SR_UNIT_CELSIUS;
-    }
-    
-    if (info->is_degf) {
-        analog->mq = SR_MQ_TEMPERATURE;
-        analog->unit = SR_UNIT_FAHRENHEIT;
-    }
-    
+	}
+	
+	if (info->is_degc) {
+		analog->mq = SR_MQ_TEMPERATURE;
+		analog->unit = SR_UNIT_CELSIUS;
+	}
+	
+	if (info->is_degf) {
+		analog->mq = SR_MQ_TEMPERATURE;
+		analog->unit = SR_UNIT_FAHRENHEIT;
+	}
+	
 	/* Measurement related flags */
 	if (info->is_ac)
 		analog->mqflags |= SR_MQFLAG_AC;
@@ -377,22 +323,22 @@ static void handle_flags(struct sr_datafeed_analog_old *analog, float *floatval,
 		analog->mqflags |= SR_MQFLAG_HOLD;
 	if (info->is_rel)
 		analog->mqflags |= SR_MQFLAG_RELATIVE;
-    if (info->is_min)
-        analog->mqflags |= SR_MQFLAG_MIN;
-    if (info->is_max)
-        analog->mqflags |= SR_MQFLAG_MAX;
-    
-
+	if (info->is_min)
+		analog->mqflags |= SR_MQFLAG_MIN;
+	if (info->is_max)
+		analog->mqflags |= SR_MQFLAG_MAX;
+	
+	
 	/* Other flags */
 	if (info->is_rs232)
 		sr_spew("RS232 enabled.");
 	if (info->is_bat)
 		sr_spew("Battery is low.");
-    if (info->is_apo)
-        sr_spew("Auto power-off mode is active.");
-    if (info->is_minmax)
-        sr_spew("Min Max mode active.");
-    if (info->is_c2c1_00)
+	if (info->is_apo)
+		sr_spew("Auto power-off mode is active.");
+	if (info->is_minmax)
+		sr_spew("Min Max mode active.");
+	if (info->is_c2c1_00)
 		sr_spew("User-defined LCD symbol 0 is active.");
 	if (info->is_c2c1_01)
 		sr_spew("User-defined LCD symbol 1 is active.");
@@ -401,9 +347,9 @@ static void handle_flags(struct sr_datafeed_analog_old *analog, float *floatval,
 SR_PRIV gboolean sr_dtm0660_packet_valid(const uint8_t *buf)
 {
 	struct dtm0660_info info;
-
+	
 	parse_flags(buf, &info);
-
+	
 	return (sync_nibbles_valid(buf) && flags_valid(&info));
 }
 
@@ -423,21 +369,21 @@ SR_PRIV gboolean sr_dtm0660_packet_valid(const uint8_t *buf)
  *         'analog' variable contents are undefined and should not be used.
  */
 SR_PRIV int sr_dtm0660_parse(const uint8_t *buf, float *floatval,
-			    struct sr_datafeed_analog_old *analog, void *info)
+							 struct sr_datafeed_analog_old *analog, void *info)
 {
 	int ret;
 	struct dtm0660_info *info_local;
-
+	
 	info_local = (struct dtm0660_info *)info;
-
+	
 	if ((ret = parse_value(buf, floatval)) != SR_OK) {
 		sr_dbg("Error parsing value: %d.", ret);
 		return ret;
 	}
-
+	
 	parse_flags(buf, info_local);
 	handle_flags(analog, floatval, info_local);
-
+	
 	return SR_OK;
 }
 
